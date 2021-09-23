9ca78eecd19fcec14c04af10c784d8c14984b9c3
Eric Guzman <ericmguzmanc@gmail.com>
2021-09-23 07:57:08 -0400

    04-expressjs

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const http = require('http');
-const routes = require('./routes');
+const path = require('path');
+const express = require("express");
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(4000);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(express.urlencoded({ extended: true }));
+/**
+ * ℹ️ To serve files statically means that the files are not being served by express but served from the file system
+ * Using express.static and middleware
+ */
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+});
+
+app.listen(4000);
+
+
